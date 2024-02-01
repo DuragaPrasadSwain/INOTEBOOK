b1db35c3a36d492117c29939657d635eba5b071d
DurgaPrasadSwain <durgaprasadswain@gmail.com>
2024-02-01 08:10:17 +0530

    published files

--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Home from './components/home';
-import About from './components/about';
-import Navbar from './components/navbar';
-import NoteState from './context/notes/noteState';
-import Alert from './components/alert';
-import AlertState from './context/alerts/alertState';
-import Login from './components/login';
-import Signin from './components/signin';
-
 
 function App() {
-  
   return (
-    <>
-    <NoteState> 
-      <AlertState>
-      <Router>
-
-        <Navbar />
-        
-        <Alert/>
-
-        <Routes>
-
-          <Route exact path="/home" element={<Home />}></Route>
-
-          <Route exact path="/about" element={<About />}></Route>
-
-          <Route exact path="/inotebook" element={<Login/>}></Route>
-
-          <Route exact path="/signin" element={<Signin/>}></Route>
-
-        </Routes>
-
-      </Router>
-      </AlertState>
-      </NoteState>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
